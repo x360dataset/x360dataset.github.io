e2d451aec3ebc49f7dad8994a0a558e3038fe9e2
x360dataset <x360dataset@outlook.com>
2024-03-14 09:52:14 +0000

    Update alpha version

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# x360dataset.github.io
+360+x: A Panoptic Multi-modal Scene Understanding Dataset - Project Page
+Welcome to our project page.
+
+Getting Started
+To begin exploring our project, we recommend starting with the index.html file. This serves as the main project page and provides an overview of our work.
+
+More Examples
+For a deeper dive into our project, we have included a range of additional examples. These are accessible in two ways:
+
+Through the Main Project Page: At the end of the index.html page, you'll find a section titled "More Examples." Clicking on the images or links in this section will redirect you to more_example.html.
+Direct Access: Alternatively, you can directly open the more_example.html file in a web browser to explore the additional examples straight away.
---
+++ static/js/video_block.js
@@ -0,0 +1,136 @@
+function video_block(categoryName_1, name_1, categoryName_2, name_2,  categoryName_3, name_3) {
+    var htmlContent = `
+<div style="display: flex; justify-content: space-between">
+<div style="width: 30%">
+
+        <h2 class="title is-4">${name_1}</h2>
+        <div id="results-carousel" class="carousel results-carousel">
+            <div class="item item-video1">
+                <center>
+                    <video poster="" id="video1_${categoryName_1}" controls height="100%">
+                        <!-- Your video file here -->
+                        <source src="static/videos/example/${categoryName_1}/360_panoramic.webm"
+                                type="video/mp4">
+                    </video>
+                </center>
+                <h2 class="subtitle has-text-centered">
+                    360° Panoramic Video Clip (temporally cropped to 10s)
+                </h2>
+            </div>
+            <div class="item item-video2">
+                <center>
+                    <video poster="" id="video2_${categoryName_1}" controls height="100%">
+                        <!-- Your video file here -->
+                        <source src="static/videos/example/${categoryName_1}/front_view.webm"
+                                type="video/mp4">
+                    </video>
+                </center>
+                <h2 class="subtitle has-text-centered">
+                    Third-person front-view Video Clip (temporally cropped to 10s)
+                </h2>
+            </div>
+            <div class="item item-video3">
+                <center>
+                    <video poster="" id="video3_${categoryName_1}" controls height="100%">
+                        <!-- Your video file here -->
+                        <source src="static/videos/example/${categoryName_1}/stereo.webm"
+                                type="video/mp4">
+                    </video>
+                </center>
+
+                <h2 class="subtitle has-text-centered">
+                    Egocentric Video Clip
+                </h2>
+            </div>
+        </div>
+    </div>
+    <div style="width: 30%">
+
+        <h2 class="title is-4">${name_2}</h2>
+        <div id="results-carousel" class="carousel results-carousel">
+            <div class="item item-video1">
+                <center>
+                    <video poster="" id="video1_${categoryName_2}" controls height="100%">
+                        <!-- Your video file here -->
+                        <source src="static/videos/example/${categoryName_2}/360_panoramic.webm"
+                                type="video/mp4">
+                    </video>
+                </center>
+                <h2 class="subtitle has-text-centered">
+                    360° Panoramic Video Clip (temporally cropped to 10s)
+                </h2>
+            </div>
+            <div class="item item-video2">
+                <center>
+                    <video poster="" id="video2_${categoryName_2}" controls height="100%">
+                        <!-- Your video file here -->
+                        <source src="static/videos/example/${categoryName_2}/front_view.webm"
+                                type="video/mp4">
+                    </video>
+                </center>
+                <h2 class="subtitle has-text-centered">
+                    Third-person front-view Video Clip (temporally cropped to 10s)
+                </h2>
+            </div>
+            <div class="item item-video3">
+                <center>
+                    <video poster="" id="video3_${categoryName_2}" controls height="100%">
+                        <!-- Your video file here -->
+                        <source src="static/videos/example/${categoryName_2}/stereo.webm"
+                                type="video/mp4">
+                    </video>
+                </center>
+
+                <h2 class="subtitle has-text-centered">
+                    Egocentric Video Clip
+                </h2>
+            </div>
+        </div>
+    </div>
+    <div style="width: 30%">
+
+        <h2 class="title is-4">${name_3}</h2>
+        <div id="results-carousel" class="carousel results-carousel">
+            <div class="item item-video1">
+                <center>
+                    <video poster="" id="video1_${categoryName_3}" controls height="100%">
+                        <!-- Your video file here -->
+                        <source src="static/videos/example/${categoryName_3}/360_panoramic.webm"
+                                type="video/mp4">
+                    </video>
+                </center>
+                <h2 class="subtitle has-text-centered">
+                    360° Panoramic Video Clip (temporally cropped to 10s)
+                </h2>
+            </div>
+            <div class="item item-video2">
+                <center>
+                    <video poster="" id="video2_${categoryName_3}" controls height="100%">
+                        <!-- Your video file here -->
+                        <source src="static/videos/example/${categoryName_3}/front_view.webm"
+                                type="video/mp4">
+                    </video>
+                </center>
+                <h2 class="subtitle has-text-centered">
+                    Third-person front-view Video Clip (temporally cropped to 10s)
+                </h2>
+            </div>
+            <div class="item item-video3">
+                <center>
+                    <video poster="" id="video3_${categoryName_3}" controls height="100%">
+                        <!-- Your video file here -->
+                        <source src="static/videos/example/${categoryName_3}/stereo.webm"
+                                type="video/mp4">
+                    </video>
+                </center>
+
+                <h2 class="subtitle has-text-centered">
+                    Egocentric Video Clip
+                </h2>
+            </div>
+        </div>
+    </div>
+    </div>
+    `;
+    return htmlContent;
+}
